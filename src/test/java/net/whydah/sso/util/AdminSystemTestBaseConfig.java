57c729da2312738bc1ba8eebec3317e7c4af7bc3
totto <totto@totto.org>
2017-09-19 20:41:54 +0200

    updated to SDK 2.3.3

--- src/test/java/net/whydah/sso/util/AdminSystemTestBaseConfig.java
+++ src/test/java/net/whydah/sso/util/AdminSystemTestBaseConfig.java
@@ -26,7 +26,7 @@ public class AdminSystemTestBaseConfig {
     public static final boolean SYSTEST_PROPERTY_ANONYMOUSTOKEN = true;
     public static final boolean SYSTEST_PROPERTY_fulltokenapplications = true;
     // Run the Whydah SystemTests?
-    public boolean systemTest = true;
+    public boolean systemTest = false;
 
     // Run SystemTests for Whydah Extensions?
     public boolean statisticsExtensionSystemTest = true && systemTest;
