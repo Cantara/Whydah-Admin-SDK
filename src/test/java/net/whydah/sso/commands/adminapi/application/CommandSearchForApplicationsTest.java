da30b237f938f3c3256dbcdaa13f86b6d1bb9cf5
totto <totto@totto.org>
2017-10-24 06:52:16 +0200

    some unittest logging cleanup

--- src/test/java/net/whydah/sso/commands/adminapi/application/CommandSearchForApplicationsTest.java
+++ src/test/java/net/whydah/sso/commands/adminapi/application/CommandSearchForApplicationsTest.java
@@ -7,6 +7,7 @@ import net.whydah.sso.commands.userauth.CommandLogonUserByUserCredential;
 import net.whydah.sso.user.helpers.UserXpathHelper;
 import net.whydah.sso.util.AdminSystemTestBaseConfig;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,7 +30,7 @@ public class CommandSearchForApplicationsTest {
     }
 
 
-    //@Ignore
+    @Ignore
     @Test
     public void testSearchApplicationsCommand() throws Exception {
         if (config.isSystemTestEnabled()) {
