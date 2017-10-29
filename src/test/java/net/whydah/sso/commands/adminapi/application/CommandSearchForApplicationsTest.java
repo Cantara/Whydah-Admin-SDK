de043656ca06f5345aded0677322629e3bbd439b
totto <totto@totto.org>
2017-10-29 14:15:23 +0100

    updated sdk dependency

--- src/test/java/net/whydah/sso/commands/adminapi/application/CommandSearchForApplicationsTest.java
+++ src/test/java/net/whydah/sso/commands/adminapi/application/CommandSearchForApplicationsTest.java
@@ -1,19 +1,17 @@
 package net.whydah.sso.commands.adminapi.application;
 
-import net.whydah.sso.application.helpers.ApplicationXpathHelper;
-import net.whydah.sso.commands.appauth.CommandLogonApplication;
 import net.whydah.sso.commands.appauth.CommandVerifyUASAccessByApplicationTokenId;
-import net.whydah.sso.commands.userauth.CommandLogonUserByUserCredential;
-import net.whydah.sso.user.helpers.UserXpathHelper;
+import net.whydah.sso.session.WhydahApplicationSession;
+import net.whydah.sso.session.WhydahUserSession;
+import net.whydah.sso.user.types.UserCredential;
 import net.whydah.sso.util.AdminSystemTestBaseConfig;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.UUID;
-
 import static net.whydah.sso.util.LoggerUtil.first50;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class CommandSearchForApplicationsTest {
@@ -21,11 +19,21 @@ public class CommandSearchForApplicationsTest {
     private static final Logger log = LoggerFactory.getLogger(CommandSearchForApplicationsTest.class);
 
     static AdminSystemTestBaseConfig config;
+    public static String userName = "admin";
+    public static String password = "whydahadmin";
+    private static WhydahApplicationSession applicationSession;
+
+
 
     @BeforeClass
     public static void setup() throws Exception {
         config = new AdminSystemTestBaseConfig();
-        //config.setLocalTest();
+        userName = config.userName;
+        password = config.password;
+        if (config.isStatisticsExtensionSystemtestEnabled()) {
+            applicationSession = WhydahApplicationSession.getInstance(config.tokenServiceUri.toString(), config.appCredential);
+        }
+
     }
 
 
@@ -33,20 +41,20 @@ public class CommandSearchForApplicationsTest {
     @Test
     public void testSearchApplicationsCommand() throws Exception {
         if (config.isSystemTestEnabled()) {
-            String myAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, config.appCredential).execute();
-            String myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
-            assertTrue(myApplicationTokenID != null && myApplicationTokenID.length() > 5);
-            String userticket = UUID.randomUUID().toString();
-            String userToken = new CommandLogonUserByUserCredential(config.tokenServiceUri, myApplicationTokenID, myAppTokenXml, config.userCredential, userticket).execute();
-            String userTokenId = UserXpathHelper.getUserTokenId(userToken);
-            assertTrue(userTokenId != null && userTokenId.length() > 5);
+            UserCredential userCredential = new UserCredential(userName, password);
+            assertTrue(applicationSession.checkActiveSession());
+            WhydahUserSession userSession = new WhydahUserSession(applicationSession, userCredential);
+            assertTrue(userSession.hasActiveSession());
+            assertNotNull(userSession.getActiveUserToken());
+            assertTrue(applicationSession.checkActiveSession());
 
-            boolean hasAccess = new CommandVerifyUASAccessByApplicationTokenId(config.userAdminServiceUri.toString(), myApplicationTokenID, userTokenId).execute();
+
+            boolean hasAccess = new CommandVerifyUASAccessByApplicationTokenId(config.userAdminServiceUri.toString(), applicationSession.getActiveApplicationTokenId(), userSession.getActiveUserTokenId()).execute();
             if (hasAccess) {
-                String applicationsJson = new CommandListApplications(config.userAdminServiceUri, myApplicationTokenID).execute();
+                String applicationsJson = new CommandListApplications(config.userAdminServiceUri, applicationSession.getActiveApplicationTokenId()).execute();
                 log.debug("applicationsJson=" + first50(applicationsJson));
 
-                String applicationsJsonl = new CommandSearchForApplications(config.userAdminServiceUri, myApplicationTokenID, userTokenId, config.appCredential.getApplicationID()).execute();
+                String applicationsJsonl = new CommandSearchForApplications(config.userAdminServiceUri, applicationSession.getActiveApplicationTokenId(), userSession.getActiveUserTokenId(), config.appCredential.getApplicationID()).execute();
                 log.debug("applicationsJson=" + first50(applicationsJsonl));
                 assertTrue(applicationsJsonl != null);
             } else {
