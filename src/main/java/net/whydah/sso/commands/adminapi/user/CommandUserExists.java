16826d6908d62892071e949f9e5d6b942f2af56f
huy <huy@DESKTOP-TRTHL1N.lan>
2021-01-21 18:07:51 +0700

    bug fix for user exists

--- src/main/java/net/whydah/sso/commands/adminapi/user/CommandUserExists.java
+++ src/main/java/net/whydah/sso/commands/adminapi/user/CommandUserExists.java
@@ -9,16 +9,16 @@ import java.net.URI;
 public class CommandUserExists extends BaseHttpGetHystrixCommandForBooleanType {
   
     private String adminUserTokenId;
-    private String userQuery;
+    private String username;
     public static int DEFAULT_TIMEOUT = 6000;
 
-    public CommandUserExists(URI userAdminServiceUri, String myAppTokenId, String adminUserTokenId, String userQuery) {
+    public CommandUserExists(URI userAdminServiceUri, String myAppTokenId, String adminUserTokenId, String username) {
         super(userAdminServiceUri, "", myAppTokenId, "UASUserAdminGroup", DEFAULT_TIMEOUT);
        
         this.adminUserTokenId = adminUserTokenId;
-        this.userQuery = userQuery;
-        if (userAdminServiceUri == null || !ApplicationTokenID.isValid(myAppTokenId) || !UserTokenId.isValid(adminUserTokenId) || adminUserTokenId.length() < 10 || userQuery == null) {
-            log.error("CommandUserExists initialized with null-values - will fail - userAdminServiceUri:{}, myAppTokenId:{}, adminUserTokenId:{}, userQuery:{}", userAdminServiceUri, myAppTokenId, adminUserTokenId, userQuery);
+        this.username = username;
+        if (userAdminServiceUri == null || !ApplicationTokenID.isValid(myAppTokenId) || !UserTokenId.isValid(adminUserTokenId) || adminUserTokenId.length() < 10 || username == null) {
+            log.error("CommandUserExists initialized with null-values - will fail - userAdminServiceUri:{}, myAppTokenId:{}, adminUserTokenId:{}, userQuery:{}", userAdminServiceUri, myAppTokenId, adminUserTokenId, username);
 
         }
 
@@ -27,7 +27,7 @@ public class CommandUserExists extends BaseHttpGetHystrixCommandForBooleanType {
         super(userAdminServiceUri, "", myAppTokenId, "UASUserAdminGroup", timeout);
        
         this.adminUserTokenId = adminUserTokenId;
-        this.userQuery = userQuery;
+        this.username = userQuery;
         if (userAdminServiceUri == null || !ApplicationTokenID.isValid(myAppTokenId) || !UserTokenId.isValid(adminUserTokenId) || adminUserTokenId.length() < 10 || userQuery == null) {
             log.error("CommandUserExists initialized with null-values - will fail - userAdminServiceUri:{}, myAppTokenId:{}, adminUserTokenId:{}, userQuery:{}", userAdminServiceUri, myAppTokenId, adminUserTokenId, userQuery);
 
@@ -43,8 +43,7 @@ public class CommandUserExists extends BaseHttpGetHystrixCommandForBooleanType {
 
 	@Override
 	protected String getTargetPath() {
-		return myAppTokenId + "/" + adminUserTokenId + "/users" + "/find/" + userQuery;
-		//return myAppTokenId + "/" + adminUserTokenId + "/users" + "/checkexist/" + userQuery;
+		return myAppTokenId + "/" + adminUserTokenId + "/users" + "/checkexist/" + username;
 	}
 
 
